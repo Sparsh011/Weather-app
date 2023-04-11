75d36a95a4d631181927047becc5bef1fa5f2dd6
Sparsh <sparsh.chadha46@gmail.com>
2023-04-12 00:19:05 +0530

    fragments set

---
+++ app/src/main/java/com/example/weatherapp/views/fragments/SearchLocationFragment.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.views.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.weatherapp.R
+
+class SearchLocationFragment : Fragment(R.layout.search_location_fragment) {
+}
